d240f78f3d066e6a3b9fcf7024d2c0627720b5f0
Carlos Navas <dante.maniacs@gmail.com>
2020-08-18 16:05:12 +0200

    adding supervisor to dictionary

--- lib/dictionary/application.ex
+++ lib/dictionary/application.ex
@@ -2,8 +2,21 @@ defmodule Dictionary.Application do
 
   use Application
 
+  alias Dictionary.WordList
+
   def start(_type, _args) do
-    Dictionary.WordList.start_link()
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(WordList, []),
+    ]
+
+    options = [
+      name: Dictionary.Supervisor,
+      strategy: :one_for_one
+    ]
+
+    Supervisor.start_link(children, options)
   end
 
 end
